34fdf4e9f036ffb6a48887a9b4bd1830fe8a0672
alvacoder-exe <martinex21@gmail.com>
2026-08-12 18:44:55 -0300

    Conectar backend con PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,31 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./presupuestos.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./presupuestos.db"
 )
+
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -14,5 +33,5 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-Base = declarative_base()
 
+Base = declarative_base()
